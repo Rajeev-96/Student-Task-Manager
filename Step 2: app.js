1da0298d3ef4ba9bab2c7e95ec4ca27185ee7b80
Rajeev Kumar <153648385+Rajeev-96@users.noreply.github.com>
2025-12-26 21:35:03 +0530

    Create Step 2: app.js

---
+++ Step 2: app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/tasks", taskRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
